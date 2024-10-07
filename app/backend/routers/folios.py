d7aef19454f20de20acd59c9aa215ff8107f4389
Jesús Cova <jesuscova@jisparking.com>
2024-10-07 07:54:32 -0300

    cambios

--- app/backend/routers/folios.py
+++ app/backend/routers/folios.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from app.backend.classes.folio_class import FolioClass
 from app.backend.db.database import get_db
 from sqlalchemy.orm import Session
+from app.backend.db.models import FolioModel
+from datetime import datetime
 
 folios = APIRouter(
     prefix="/folios",
@@ -13,3 +15,29 @@ def get(branch_office_id:int, cashier_id:int, quantity:int, db: Session = Depend
     data = FolioClass(db).get(branch_office_id, cashier_id, quantity)
 
     return {"message": data}
+
+
+@folios.get("/caf")
+def caf(db: Session = Depends(get_db)):
+    # Define el rango de folios
+    folio_start = 16977684
+    folio_end = 17021050
+    current_date = datetime.now().strftime('%Y-%m-%d')
+
+    # Iterar sobre el rango y realizar la inserción para cada folio
+    for folio_number in range(folio_start, folio_end + 1):
+        new_folio = FolioModel(
+            folio=folio_number,
+            branch_office_id=0,
+            cashier_id=0,
+            requested_status_id=0,
+            used_sattus_id=0,
+            added_date=current_date,
+            updated_date=current_date
+        )
+        db.add(new_folio)
+
+    # Confirmar todos los cambios después del bucle
+    db.commit()
+
+    return {"message": f"Inserted folios from {folio_start} to {folio_end}"}
