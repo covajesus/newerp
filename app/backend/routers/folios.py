61d95485d2bf06df3c41124a896ad939adc93dac
Jesús Cova <jesuscova@jisparking.com>
2025-02-11 20:06:56 -0300

    cambios

--- app/backend/routers/folios.py
+++ app/backend/routers/folios.py
@@ -20,7 +20,7 @@ def get_all_folios(folio: FolioList, db: Session = Depends(get_db)):
 def get(branch_office_id:int, cashier_id:int, requested_quantity:int, quantity_in_cashier:int, db: Session = Depends(get_db)):
     data = FolioClass(db).get(branch_office_id, cashier_id, requested_quantity, quantity_in_cashier)
 
-    return {"message": data}
+    return {"message": ''}
 
 @folios.get("/update/{folio}")
 def update(folio:int, db: Session = Depends(get_db)):
