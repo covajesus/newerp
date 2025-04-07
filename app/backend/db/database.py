d26bbf5d5ca9e14278b1a4d997f46a4a6877fb1d
Jesús Cova <jesuscova@jisparking.com>
2025-04-07 19:30:21 -0400

    cambios

--- app/backend/db/database.py
+++ app/backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Chile20255!@jisbackend.com:3306/jisparking"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:Chile20255!@localhost:3306/jisparking"
 
 # Crear el motor con echo=True para activar el registro de consultas
 engine = create_engine(SQLALCHEMY_DATABASE_URI, pool_size=20, max_overflow=0, echo=False)
