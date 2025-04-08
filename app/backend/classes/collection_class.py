d4878f39d12e616c9f3dcdf309bd58c94a59a2d3
Jesús Cova <jesuscova@jisparking.com>
2025-04-08 11:32:58 -0400

    cambios

--- app/backend/classes/collection_class.py
+++ app/backend/classes/collection_class.py
@@ -285,13 +285,14 @@ class CollectionClass:
                 error_message = str(e)
                 return f"Error: {error_message}"
         else:
-            check_collection = self.db.query(CollectionModel.id, CollectionModel.cash_gross_amount, CollectionModel.cash_net_amount, CollectionModel.card_gross_amount, CollectionModel.card_net_amount, CollectionModel.total_tickets).filter(
+            check_collection = self.db.query(CollectionModel).filter(
                 CollectionModel.cashier_id == collection_inputs['cashier_id'],
                 CollectionModel.branch_office_id == collection_inputs['branch_office_id'],
                 CollectionModel.added_date == collection_inputs['added_date']
             ).first()
             
-            print(check_collection.cash_gross_amount)
+            print(CollectionModel.added_date)
+            print(collection_inputs['added_date'])
             
             if check_collection.cash_gross_amount != collection_inputs['cash_gross_amount'] or check_collection.card_gross_amount != collection_inputs['card_gross_amount']:
                 print(check_collection.id)
