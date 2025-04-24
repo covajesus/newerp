7804ca722573dca49c6d82505028ebf80b739b50
Jesús Cova <jesuscova@jisparking.com>
2025-04-24 13:46:43 -0400

    cambios

--- app/backend/routers/authentications.py
+++ app/backend/routers/authentications.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Session
 from app.backend.classes.authentication_class import AuthenticationClass
 from app.backend.classes.rol_class import RolClass
 from datetime import timedelta
+from app.backend.auth.auth_user import get_current_active_user
+from app.backend.schemas import UserLogin
 import json
 
 authentications = APIRouter(
@@ -45,4 +47,28 @@ def logout(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
         "rol_id": user.rol_id,
         "nickname": user.nickname,
         "token_type": "bearer"
+    }
+
+@authentications.post("/refresh")
+def refresh_token(
+    db: Session = Depends(get_db),
+    session_user: UserLogin = Depends(get_current_active_user)
+):
+    # Generar nuevo token con misma data de usuario
+    token_expires = timedelta(minutes=30)
+    token = AuthenticationClass(db).create_token({'sub': str(session_user.rut)}, token_expires)
+    expires_in_seconds = token_expires.total_seconds()
+
+    # También puedes retornar información adicional si la necesitas
+    rol = RolClass(db).get('id', session_user.rol_id)
+
+    return {
+        "access_token": token,
+        "rut": session_user.rut,
+        "rol_id": session_user.rol_id,
+        "rol": rol.rol,
+        "full_name": session_user.full_name,
+        "email": session_user.email,
+        "token_type": "bearer",
+        "expires_in": expires_in_seconds
     }
