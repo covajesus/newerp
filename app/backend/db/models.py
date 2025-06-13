ecc8d261c81fd978191644174640f00f5f89ffa9
Jesús Cova <jesuscova@jisparking.com>
2025-06-12 20:32:24 -0400

    cambios

--- app/backend/db/models.py
+++ app/backend/db/models.py
@@ -312,6 +312,8 @@ class FolioReportModel(Base):
     __tablename__ = 'folio_report'
     id = Column(Integer, primary_key=True, autoincrement=True)
     cashier = Column(String(255))
+    rustdesk = Column(String(255))
+    anydesk = Column(String(255))
 
 class CashierModel(Base):
     __tablename__ = 'cashiers'
