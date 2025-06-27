803c12c3ba02e1752f50e08af673a1b59ca57054
Jesús Cova <jesuscova@jisparking.com>
2025-06-27 12:09:00 -0400

    cambios

--- app/backend/routers/collections.py
+++ app/backend/routers/collections.py
@@ -75,10 +75,10 @@ def post(update_collection: UpdateCollection, db: Session = Depends(get_db)):
 
 @collections.get("/cron")
 def cron(db: Session = Depends(get_db), db2: Session = Depends(get_db2)):
-    data = CollectionClass(db).get_all_collections()
+    data = CollectionClass(db2).get_all_collections()
 
     print(data)
 
-    CollectionClass(db2).update_all_collections(data)
+    CollectionClass(db).update_all_collections(data)
 
     return {"message": 'Updated o inserted collections in the second database.'}
